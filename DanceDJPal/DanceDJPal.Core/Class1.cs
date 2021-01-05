5d2f3ba609c21e5b69862608b8ed8f2a388853ad
ShaWn <nonyabizness@gmail.com>
2021-01-04 22:06:07 -0600

    Tap for BPM success!

---
+++ DanceDJPal/DanceDJPal.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DanceDJPal.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ DanceDJPal/DanceDJPal/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DanceDJPal.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string bpmText;
+        public string BPMText
+        {
+            get => bpmText;
+            set => this.RaiseAndSetIfChanged(ref bpmText, value);
+        }
+        private DateTime _FirstClickTime;
+        private int _ClickCount;
+
+        public MainWindowViewModel()
+        {
+            BPMText = "BPM will appear here.";
+        }
+
+        public void TapForBPMCommand()
+        {
+            if(_FirstClickTime == DateTime.MinValue)
+            {
+                _FirstClickTime = DateTime.Now;                
+            }
+            _ClickCount++;
+            var timeSinceFirstClick = DateTime.Now - _FirstClickTime;
+            var bpm = _ClickCount / timeSinceFirstClick.TotalMinutes;
+            BPMText = bpm.ToString() + " BPM";
+        }
+
+        public void ResetCommand()
+        {
+            _ClickCount = 0;
+            _FirstClickTime = DateTime.MinValue;
+        }
+    }
+}
